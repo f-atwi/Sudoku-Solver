e704acb624d7d7048b71cf5d4517ddf2a6896dac
Fouad Atwi <atwe98@gmail.com>
2022-03-27 23:31:54 +0200

    Created sudoku solver class with useful functions

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 class SudokuSolver:
     def __init__(self) -> None:
         self.sudoku = [[[i for i in range(1, 10)]
@@ -30,12 +28,12 @@ class SudokuSolver:
                 row[y].remove(value)
 
     def removeFromBlock(self, x, y, value):
-        for i in range(x - x% 3, x - x%3 + 3):
+        for i in range(x - x % 3, x - x % 3 + 3):
             for j in range(y - y % 3, y - y % 3 + 3):
                 if i != x and j != y and value in self.sudoku[i][j]:
                     self.sudoku[i][j].remove(value)
 
-    def iterate(self):
+    def eliminatePossibilities(self):
         definites = self.getDefinites()
         for i, j, k in definites:
             self.removeFromRow(i, j, k)
@@ -49,6 +47,24 @@ class SudokuSolver:
                 if len(cell) == 1:
                     definites.append([i, j, cell[0]])
         return definites
+
+    def rowSolver(self):
+        for i, row in enumerate(self.sudoku):
+            for j, cell in enumerate(row):
+                for p in cell:
+                    if len([c for c in row if p in c]) == 1:
+                        self.removeFromRow(i, j, p)
+
+    # def colSolver(self):
+    #     for j in range(9):
+    #         union = [p for ]
+
+    #     for i, row in enumerate(self.sudoku):
+    #         union = [p for cell in row for p in cell]
+    #         for p in union:
+    #             if union.count(p)==1:
+    #                 j=[j for j, cell in enumerate(row) if p in cell][0]
+    #                 self.removeFromCol(i,j,p)
 
     def isSolved(self) -> bool:
         return all([all(len(cell) == 1 for cell in row) for row in self.sudoku])
@@ -95,19 +111,18 @@ class SudokuSolver:
 def main():
     sudoku = SudokuSolver()
     sudoku.defineHints([[0, 1, 2], [0, 4, 3], [0, 7, 4], [1, 0, 6], [1, 8, 3], [2, 2, 4], [2, 6, 5], [3, 3, 8], [3, 5, 6], [4, 0, 8], [
-                         4, 4, 1], [4, 8, 6], [5, 3, 7], [5, 5, 5], [6, 2, 7], [6, 6, 6], [7, 0, 4], [7, 8, 8], [8, 1, 3], [8, 4, 4], [8, 7, 2]])
+        4, 4, 1], [4, 8, 6], [5, 3, 7], [5, 5, 5], [6, 2, 7], [6, 6, 6], [7, 0, 4], [7, 8, 8], [8, 1, 3], [8, 4, 4], [8, 7, 2]])
     # sudoku.defineHints([[2,2,4]])
     print(sudoku)
-    i =0
-    while not sudoku.isSolved() or s==sudoku:
-        sudoku.iterate()
-        print(repr(sudoku))
+    print(repr(sudoku))
 
-        input("Press Enter to continue...")
+    sudoku.eliminatePossibilities()
 
-        i+=1
-    
-    print(i)
+    # sudoku.rowSolver()
+    # sudoku.colSolver()
+
+    print(repr(sudoku))
+
 
 if __name__ == "__main__":
     main()
